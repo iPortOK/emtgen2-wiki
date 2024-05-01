58e907cb317a656d23457d6246bcc675a952648c
iPortOK <127425381+iPortOK@users.noreply.github.com>
2024-05-01 13:48:22 +0200

    ok

--- src/components/kov/aura.jsx
+++ src/components/kov/aura.jsx
@@ -2,22 +2,22 @@ import { Container, Typography, Box, Table, TableBody, TableCell, TableContainer
 import './textbox.css';
 import React, { useState, useEffect } from 'react';
 
-const DračíKamenTable = ({ data }) => {
+const AuraTable = ({ data }) => {
   return (
     <TableContainer className='textbox2'>
       <Table>
         <TableBody>
           {data.map((row, index) => (
             <TableRow key={index}>
-              <TableCell className='spodnicastfix' align="center" component="th" scope="row" style={{ verticalAlign: 'middle' }}>
+              <TableCell className='spodnicastfix hello' align="center" component="th" scope="row" style={{ verticalAlign: 'middle' }}>
                 <div className="cell-container">
                   <img className='obrz' src={row.imgIcon} alt={row.nazev} />
                   <span className="cell-text boxnadpis3">{row.nazev}</span>
                 </div>
               </TableCell>
-              <TableCell align="right" className='spodnicastfix'>
-                {row.bonusy.map((bonus, bonusIndex) => (
-                  <div className='boxnadpis3' key={bonusIndex}>{bonus}</div>
+              <TableCell align="left" className='spodnicastfix hello2'>
+              {row.bonusy.map((bonus, bonusIndex) => (
+                  <div className='boxnadpis4' key={bonusIndex} style={{ color: row.barvy[bonusIndex] }}>{bonus}</div>
                 ))}
               </TableCell>
             </TableRow>
@@ -44,94 +44,97 @@ const Aura = () => {
     }
   }, [activeIndex]);
 
-  // Data pro Dračí Kameny
-  const dračíKamenyData = [
+  const auraData = [
     { 
-      nazev: 'Legendární diamant',
-      imgIcon: 'https://img.m2icondb.com/114400.png',
-      bonusy: ['Inteligence', 'Odolnost proti ledu', 'Odolnost proti ledu', 'Odolnost proti ledu']
+      nazev: 'Ledová runa s aurou',
+      imgIcon: 'https://img.m2icondb.com/49990.png',
+      bonusy: ['Item sloužící k vylepšování kostýmu s aurou', 'Lze jej získat v mapě od úrovně 115'],
+      barvy: ['', '', '']
     },
     { 
-      nazev: 'Mýtická diamant',
-      imgIcon: 'https://img.m2icondb.com/115400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
+      nazev: 'Běžný kostým s aurou',
+      imgIcon: 'https://img.m2icondb.com/49001.png',
+      bonusy: ['Hodnota absorbace: 1%', 'Magický útok/útok zbraní +1%', 'Úroveň aury: 0-49'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Legendární rubín',
-      imgIcon: 'https://img.m2icondb.com/123400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
+      nazev: 'Prostý kostým s aurou',
+      imgIcon: 'https://img.m2icondb.com/49002.png',
+      bonusy: ['Hodnota absorbace: 5%', 'Magický útok/útok zbraní +2%', 'Úroveň aury: 50-99'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Mýtická rubín',
-      imgIcon: 'https://img.m2icondb.com/125400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
+      nazev: 'Vznešený kostým s aurou',
+      imgIcon: 'https://img.m2icondb.com/49003.png',
+      bonusy: ['Hodnota absorbace: 10%', 'Magický útok/útok zbraní +4%', 'Úroveň aury: 100-149'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Legendární nefrit',
-      imgIcon: 'https://img.m2icondb.com/134400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
+      nazev: 'Zářivý kostým s aurou',
+      imgIcon: 'https://img.m2icondb.com/49004.png',
+      bonusy: ['Hodnota absorbace: 15%', 'Magický útok/útok zbraní +6%', 'Úroveň aury: 150-199'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Mýtická nefrit',
-      imgIcon: 'https://img.m2icondb.com/135400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
+      nazev: 'Úžasný kostým s aurou',
+      imgIcon: 'https://img.m2icondb.com/49005.png',
+      bonusy: ['Hodnota absorbace: 20%', 'Magický útok/útok zbraní +8%', 'Úroveň aury: 200-249'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Legendární safír',
-      imgIcon: 'https://img.m2icondb.com/144400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
+      nazev: 'Zářivý kostým s aurou',
+      imgIcon: 'https://img.m2icondb.com/49006.png',
+      bonusy: ['Hodnota absorbace: 25%', 'Magický útok/útok zbraní +10%', 'Úroveň aury: 250'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Mýtická safír',
-      imgIcon: 'https://img.m2icondb.com/145400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
+      nazev: 'Zesilovač aury (3%)',
+      imgIcon: './items/Zesilovač3.png',
+      bonusy: ['Hodnota absorbace: +3%', 'Lze vyrobit u pirátského čističe.'],
+      barvy: ['', '', '']
     },
     { 
-      nazev: 'Legendární granát',
-      imgIcon: 'https://img.m2icondb.com/154400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
+      nazev: 'Zesilovač aury (5%)',
+      imgIcon: './items/Zesilovač5.png',
+      bonusy: ['Hodnota absorbace: +5%', 'Lze vyrobit u pirátského čističe.'],
+      barvy: ['', '', '']
     },
     { 
-      nazev: 'Mýtická granát',
-      imgIcon: 'https://img.m2icondb.com/155400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
-    },
-    { 
-      nazev: 'Legendární onyx',
-      imgIcon: 'https://img.m2icondb.com/164400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
-    },
-    { 
-      nazev: 'Mýtická onyx',
-      imgIcon: 'https://img.m2icondb.com/165400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
-    },
-    { 
-      nazev: 'Legendární ametyst',
-      imgIcon: 'https://img.m2icondb.com/174400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
-    },
-    { 
-      nazev: 'Mýtická ametyst',
-      imgIcon: 'https://img.m2icondb.com/175400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
+      nazev: 'Zesilovač aury (10%)',
+      imgIcon: './items/Zesilovač10.png',
+      bonusy: ['Hodnota absorbace: +10%', 'Lze dropnout z Worldbosse, který se nachází na ostrově přeludů.'],
+      barvy: ['', '', '']
     },
   ];
 
   return (
     <Container className='main'>
-      <Typography className="boxnadpis1">Dračí Alchymie</Typography>
+      <Typography className="boxnadpis1">Kostým s aurou</Typography>
       <Box className="textbox">
-        <Typography className="boxnadpis2">Druhy Dračích Kamenů:</Typography>
+        <Typography className="boxnadpis2">Základní informace:</Typography>
         <Typography paragraph className="text1">
-          Existuje sedm druhů Dračích Kamenů, každý s unikátními bonusy a vlastnostmi:
+        Kostým s aurou patří k systémům, se kterými se setkáváme až v pozdější části hry. Lze ho používát od úrovně 90+, avšak material na vylepšování lze získat až od úrovně 115.
+        Kostým s aurou funguje na stejném principu jako šerpa, s tím rozdílem, že místo absorbace zbraně absorbujete buď štít, náhrdelník, náušnice nebo náramek.
+        Navíc, jako bonus, má kostým s aurou v základu procentuální Magický útok / útok zbraní. Hodnota se zvyšuje s úrovní aury. S úrovní aury se mění i její vzhled.
         </Typography>
-        <div></div>
+       
+        <Typography className="boxnadpis2">Výroba:</Typography>
+        <Typography paragraph className="text1">
+        Vytvoření <img src="https://img.m2icondb.com/49001.png" alt="" className=''></img>běžného kostýmu s aurou je možné u Spisovatele.<br></br>
+          <Box className="uppkybox">
+        &#x2022; 25x <img src="./items/Oxid titaničitý.png" alt="" className=''></img> Oxid titaničitý <br></br>
+        &#x2022; 25x <img src="./items/Šupiny Hydry.png" alt="" className=''></img> Šupiny Hydry <br></br>
+        &#x2022; 25x <img src="./items/Ztracený list.png" alt="" className=''></img> Ztracený list <br></br>
+        &#x2022; 300x <img src="./items/Zelená dračí fazole.png" alt="" className=''></img> Zelená dračí fazole <br></br>
+        &#x2022; 100x <img src="./items/Kus Šerpy.png" alt="" className=''></img> Kus Šerpy<br></br>
+        &#x2022; <img src="./items/Yang.png" alt="" className=''></img> 2.000.000.000 yangů
+        </Box>
+        </Typography>
       </Box>
 
       <div>
         <Box className="vedle">
-          {dračíKamenyData.map((kámen, index) => (
+          {auraData.map((kámen, index) => (
             <div key={index} className={`boxik velikost ${index === activeIndex ? 'selected' : ''}`} onClick={() => switchMap(index)}>
               <div className='fixik'>
                 <img src={kámen.imgIcon} alt={kámen.nazev} />
@@ -140,9 +143,12 @@ const Aura = () => {
           ))}
         </Box>
       </div>
-      {activeIndex !== null && <DračíKamenTable data={[dračíKamenyData[activeIndex]]} />} {/* Zobrazí tabulku pouze pokud je aktivní index nenulový */}
+      {activeIndex !== null && <AuraTable data={[auraData[activeIndex]]} />}
+    
     </Container>
   );
 };
+
+
 
 export default Aura;
--- src/components/kov/serpa.jsx
+++ src/components/kov/serpa.jsx
@@ -2,22 +2,22 @@ import { Container, Typography, Box, Table, TableBody, TableCell, TableContainer
 import './textbox.css';
 import React, { useState, useEffect } from 'react';
 
-const DračíKamenTable = ({ data }) => {
+const SerpaTable = ({ data }) => {
   return (
     <TableContainer className='textbox2'>
       <Table>
         <TableBody>
           {data.map((row, index) => (
             <TableRow key={index}>
-              <TableCell className='spodnicastfix' align="center" component="th" scope="row" style={{ verticalAlign: 'middle' }}>
+              <TableCell className='spodnicastfix hello' align="center" component="th" scope="row" style={{ verticalAlign: 'middle' }}>
                 <div className="cell-container">
                   <img className='obrz' src={row.imgIcon} alt={row.nazev} />
                   <span className="cell-text boxnadpis3">{row.nazev}</span>
                 </div>
               </TableCell>
-              <TableCell align="right" className='spodnicastfix'>
-                {row.bonusy.map((bonus, bonusIndex) => (
-                  <div className='boxnadpis3' key={bonusIndex}>{bonus}</div>
+              <TableCell align="left" className='spodnicastfix hello2'>
+              {row.bonusy.map((bonus, bonusIndex) => (
+                  <div className='boxnadpis4' key={bonusIndex} style={{ color: row.barvy[bonusIndex] }}>{bonus}</div>
                 ))}
               </TableCell>
             </TableRow>
@@ -44,94 +44,59 @@ const Serpa = () => {
     }
   }, [activeIndex]);
 
-  // Data pro Dračí Kameny
-  const dračíKamenyData = [
+  const serpaData = [
     { 
-      nazev: 'Legendární diamant',
-      imgIcon: 'https://img.m2icondb.com/114400.png',
-      bonusy: ['Inteligence', 'Odolnost proti ledu', 'Odolnost proti ledu', 'Odolnost proti ledu']
+      nazev: 'Obyčejné',
+      imgIcon: 'https://img.m2icondb.com/85001.png',
+      bonusy: ['Hodnota absorbace: 1%'],
+      barvy: ['', '', '']
     },
     { 
-      nazev: 'Mýtická diamant',
-      imgIcon: 'https://img.m2icondb.com/115400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
+      nazev: 'Jemné',
+      imgIcon: 'https://img.m2icondb.com/85002.png',
+      bonusy: ['Hodnota absorbace: 5%'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Legendární rubín',
-      imgIcon: 'https://img.m2icondb.com/123400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
+      nazev: 'Noblesní',
+      imgIcon: 'https://img.m2icondb.com/85003.png',
+      bonusy: ['Hodnota absorbace: 10%'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Mýtická rubín',
-      imgIcon: 'https://img.m2icondb.com/125400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
+      nazev: 'Uživatelské',
+      imgIcon: 'https://img.m2icondb.com/85004.png',
+      bonusy: ['Hodnota absorbace: 11-25%'],
+      barvy: ['', '', '#dfd8c6']
     },
     { 
-      nazev: 'Legendární nefrit',
-      imgIcon: 'https://img.m2icondb.com/134400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
+      nazev: 'Zesilovač šerpy (5%)',
+      imgIcon: './items/ZesilovačŠ.png',
+      bonusy: ['Hodnota absorbace: +5%'],
+      barvy: ['', '', '#dfd8c6']
     },
-    { 
-      nazev: 'Mýtická nefrit',
-      imgIcon: 'https://img.m2icondb.com/135400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
-    },
-    { 
-      nazev: 'Legendární safír',
-      imgIcon: 'https://img.m2icondb.com/144400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
-    },
-    { 
-      nazev: 'Mýtická safír',
-      imgIcon: 'https://img.m2icondb.com/145400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
-    },
-    { 
-      nazev: 'Legendární granát',
-      imgIcon: 'https://img.m2icondb.com/154400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
-    },
-    { 
-      nazev: 'Mýtická granát',
-      imgIcon: 'https://img.m2icondb.com/155400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
-    },
-    { 
-      nazev: 'Legendární onyx',
-      imgIcon: 'https://img.m2icondb.com/164400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
-    },
-    { 
-      nazev: 'Mýtická onyx',
-      imgIcon: 'https://img.m2icondb.com/165400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
-    },
-    { 
-      nazev: 'Legendární ametyst',
-      imgIcon: 'https://img.m2icondb.com/174400.png',
-      bonusy: ['+10% šance na kritický zásah', '+5 obrana']
-    },
-    { 
-      nazev: 'Mýtická ametyst',
-      imgIcon: 'https://img.m2icondb.com/175400.png',
-      bonusy: ['+15% šance na kritický zásah', '+10 obrana']
-    },
+    
   ];
 
   return (
     <Container className='main'>
-      <Typography className="boxnadpis1">Dračí Alchymie</Typography>
+      <Typography className="boxnadpis1">Systém Šerp</Typography>
       <Box className="textbox">
-        <Typography className="boxnadpis2">Druhy Dračích Kamenů:</Typography>
+        <Typography className="boxnadpis2">Co jsou šerpy?</Typography>
         <Typography paragraph className="text1">
-          Existuje sedm druhů Dračích Kamenů, každý s unikátními bonusy a vlastnostmi:
+        Podobně jako kostýmy, i šerpy můžou propůjčovat postavám bonusy, které vylepšují jejich vlastnosti. Na rozdíl od kostýmů ale šerpy tyto bonusy nezískávají automaticky.
+        Tyto bonusy se aktivují absorbováním jiného předmětu.
         </Typography>
-        <div></div>
-      </Box>
+        <Typography className="boxnadpis2">Vylepšování šerp</Typography>
+        <Typography paragraph className="text1">
+        Šerpy se dají vylepšovat do 4 úrovní kvality. Pro dosažení vyšší úrovně je potřeba sloučit dvě šerpy o úroveň nižší.<br></br><br></br>
+        &times; Při kombinaci šerp je šance na neúspěch, v takovém případě se spodní šerpa zničí.<br></br>
+        &times; Uživatelské šerpy se dají dále vylepšovat pro navýšení absorpčního bonusu kombinací s jinou uživatelskou šerpou. Riziko neúspěchu nehrozí, šerpa si ponechá stávající bonus nebo se o 1-5% zlepší (maximálně na 25%).
+        </Typography>
 
-      <div>
+        <div>
         <Box className="vedle">
-          {dračíKamenyData.map((kámen, index) => (
+          {serpaData.map((kámen, index) => (
             <div key={index} className={`boxik velikost ${index === activeIndex ? 'selected' : ''}`} onClick={() => switchMap(index)}>
               <div className='fixik'>
                 <img src={kámen.imgIcon} alt={kámen.nazev} />
@@ -140,9 +105,48 @@ const Serpa = () => {
           ))}
         </Box>
       </div>
-      {activeIndex !== null && <DračíKamenTable data={[dračíKamenyData[activeIndex]]} />} {/* Zobrazí tabulku pouze pokud je aktivní index nenulový */}
+      {activeIndex !== null && <SerpaTable data={[serpaData[activeIndex]]} />}
+
+        <Typography className="boxnadpis2"><br></br>Absorpce bonusů</Typography>
+        <Typography paragraph className="text1">
+        Šerpy můžou absorbovat část vlastností z jiného předmětu (brnění, zbraně). Množství absorbovaných vlastností závisí na typu šerpy a její úrovni:
+        </Typography>
+
+
+      <Typography paragraph className="text1">
+      &times; Absorbované vlastnosti nelze upravovat. Jediná možnost je je smazat certifikátem zrušení a absorbovat je znovu.
+      </Typography>
+
+      <Typography className="boxnadpis2">Hodnota absorpce bonusů</Typography>
+        <Typography paragraph className="text1">
+        Každá šerpa má unikátní hodnotu absorpce bonusů, která určuje, kolik procent z původního bonusu předmětu bude zachováno.<br></br><br></br>
+        <b>Příklad:</b><br></br>
+        &times; Šerpa mistra (noblesní) s 10% absorpčním bonusem<br></br>
+        &times; Úplňkový meč s 30% průměrnou škodou, -11% poškození schopností a o 20% silnější proti nemrtvým<br></br><br></br>
+        <b>Výsledek:</b><br></br>
+        &times; Průměrná škoda: 10% z 30% = 3%<br></br>
+        &times; Poškození schopností: 10% z -11% = -1,1% (zaokrouhleno na 1%)<br></br>
+        &times; Síla proti nemrtvým: 10% z 20% = 2%
+        </Typography>
+
+        <Typography className="boxnadpis2">Výroba:</Typography>
+        <Typography paragraph className="text1">
+        Vytvoření <img src="https://img.m2icondb.com/85001.png" alt="" className=''></img> obyčejné šerpy je možné u Theowahdana.<br></br>
+        <Box className="uppkybox">
+        &#x2022; 1x <img src="./items/Kus Šerpy.png" alt="" className=''></img> Kus Šerpy <br></br>
+        &#x2022; 25x <img src="./items/Runa III.png" alt="" className=''></img> Runa III. <br></br>
+        &#x2022; 5x <img src="./items/Magický kámen.png" alt="" className=''></img> Magický kámen <br></br>
+        &#x2022; 5x <img src="./items/Plán zbroje.png" alt="" className=''></img> Plán zbroje <br></br>
+        &#x2022; 4x <img src="./items/Božské ovoce.png" alt="" className=''></img> Božské ovoce<br></br>
+        &#x2022; <img src="./items/Yang.png" alt="" className=''></img>10.000.000 yangů
+        </Box>   
+        </Typography>
+      </Box>
+    
     </Container>
   );
 };
+
+
 
 export default Serpa;
